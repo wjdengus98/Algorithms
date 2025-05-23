92ad7b1cc81e26b2e128249b7aefd10fcad3a100
wjgusen98 <jhd6290@gmail.com>
2025-05-23 23:59:23 +0900

    feat: 백준 9237번(이장님 초대) 문제 풀이

---
+++ Greedy/BOJ 9237.py
@@ -0,0 +1,11 @@
+n = int(input())
+trees = list(map(int,input().split()))
+trees.sort(reverse=True)
+
+max_day = 0
+for i in range(len(trees)):
+    grow_day = i + trees[i]
+    if grow_day > max_day:
+        max_day = grow_day
+
+print(max_day + 1)  # 마지막 나무 자란 다음 날이 파티 날

56718e0fe05107e677843b5563a66763ead1c68b
Hyundoo Jeong <jhd6290@gmail.com>
2026-06-23 23:53:29 +0900

    [level 1] Title: 택배 상자 꺼내기, Time: 0.05 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.py
+++ 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.py
@@ -1,3 +1,5 @@
+import math
+
 def solution(n,w,num):
     h = (n // w) + 1
     storages = []
@@ -11,27 +13,11 @@ def solution(n,w,num):
                 val += 1
             else:
                 box.append(0)
-        
-        if i % 2 == 1: # i = 1, 3일 때 역순으로 뒤집어 지그재그 구현하기
-            box.reverse()
-        
         storages.append(box)
     
 
     # 밑으로 추적하기
-    target_r, target_c = -1,-1
-    for r in range(h):
-        for c in range(w):
-            if storages[r][c] == num:
-                target_r = r
-                target_c = c
-                break
-        if target_r != -1:
-            break
-    
     cnt = 0
-    for r in range(target_r, h):
-        if storages[r][target_c] != 0:
-            cnt += 1
     
-    return cnt
+    
+    return storages
